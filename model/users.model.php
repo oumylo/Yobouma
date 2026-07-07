402a4548e2ef5e2748918e3b3f0415df8ecb0af7
oumylo <oumylo611@hmail.com>
2026-07-07 02:19:06 +0000

    feat(auth): ajout du modèle User et de la gestion de deux type d'utilisateur

---
+++ model/users.model.php
@@ -0,0 +1,8 @@
+<?php
+
+$users = [
+
+    ["id" => 1,"nom" => "Oumy","telephone" => "778122604","password" => "1234","role" => "passager"],
+    ["id" => 2,"nom" => "Moussa","telephone" => "773237034","password" => "1234","role" => "chauffeur"]
+
+];
